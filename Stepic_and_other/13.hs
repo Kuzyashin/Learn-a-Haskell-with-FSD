0cfece8048fe627439299a62992668f7706db004
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2018-05-11 18:01:20 +0300

    commit

---
+++ Stepic_and_other/13.hs
@@ -0,0 +1,8 @@
+module Demo where
+
+import Data.Function
+
+sumFstFst = (+) `on` helper
+    where helper pp = fst $ fst pp
+
+sumFstFst2 = (+) `on` (\pp -> fst $ fst pp)
---
+++ Stepic_and_other/15.hs
@@ -0,0 +1,9 @@
+module Demo where
+
+doItYourself = f . g . h
+
+f = logBase 2
+    
+g = (^3)
+    
+h = max 42
